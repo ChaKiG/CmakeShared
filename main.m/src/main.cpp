97f104ee844b5c1081f625fb149aab26b44b3804
TPlltr <thomas.pelletier45@hotmail.fr>
2020-02-15 19:26:10 +0100

    final ?

---
+++ main.m/src/main.cpp
@@ -0,0 +1,13 @@
+#include "lib1help.h"
+#include "lib2help.h"
+#include <cstdlib>
+
+int main()
+{
+    Lib1Help h1;
+    h1.Lib1Help_Print();
+
+    Lib2Help h2;
+    h2.Lib2Help_Print();
+    return 0;
+}

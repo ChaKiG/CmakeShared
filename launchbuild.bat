4567309d17b4e31352e2e3d25e12eac868f3a14c
Thomas PELLETIER <thomas.pelletier@3ds.com>
2020-03-16 12:13:54 +0100

    finally !

--- launchbuild.bat
+++ launchbuild.bat
@@ -2,13 +2,12 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 
 set "CC=clang"
 set "CXX=clang++"
-REM set "CC=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\Hostx64\x64\cl.exe"
-REM set "CXX=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.24.28314\bin\Hostx64\x64\cl.exe"
-
+REM set "CC=cl"
+REM set "CXX=cl"
 
 rd /s /q build
 rd /s /q install
-mkdir build
-mkdir install
-cmake . -G "Ninja" -B build
-ninja -C build
+
+cmake -GNinja -B build -DCMAKE_INSTALL_PREFIX=.\install
+cmake --build build
+cmake --instal build
